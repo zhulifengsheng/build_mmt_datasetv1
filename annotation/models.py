67b7caf3f76328edde2b79d57323e5bb81e5c851
zhulifengsheng <18240091639@163.com>
2022-11-29 21:02:55 +0800

    init

---
+++ annotation/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# create database mmt_databasev1 DEFAULT CHARSET utf8 COLLATE utf8_general_ci;
+# python3 manage.py makemigrations --empty annotation
+# python3 manage.py makemigrations
+# python3 manage.py migrate
+
+# alter table annotation_caption1 auto_increment 1;
+
+# Create your models here.
