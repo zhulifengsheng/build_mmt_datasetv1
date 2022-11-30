897caa0d774aa557fc62df3ac9d77ba0ecc236d2
zhulifengsheng <18240091639@163.com>
2022-11-30 17:36:32 +0800

    11.30

--- annotation/views/management.py
+++ annotation/views/management.py
@@ -1,25 +1,9 @@
 from django.shortcuts import render
 from django.http import Http404
 
-from annotation.models import User, Caption
+from annotation.models import User
 
 def management(request):
-
-    # f1 = open('D:\\LEARN\\datasets\\COCO2017\\captions.txt', 'r', encoding='utf-8')
-    # f2 = open('D:\\LEARN\\datasets\\COCO2017\\captions_translation.txt', 'r', encoding='utf-8')
-    # for l1, l2 in zip(f1, f2):
-    #     # caption_id = models.PositiveIntegerField(verbose_name='Caption ID', primary_key=True)
-    #     # caption_NO = models.PositiveSmallIntegerField(verbose_name='第几个描述')    # 1-7
-    #     # caption = models.TextField(verbose_name='英文描述')
-    #     # zh_machine_translation = models.TextField(verbose_name='机器翻译')
-    #     # is_ambiguity = models.BooleanField(verbose_name='改英文是否歧义', default=False)
-
-    #     # # 该caption链接到那个图片
-    #     # image_obj = models
-    #     Caption.objects.create()
-    #     print(l1, l2)
-    #     break
-
     user_obj = User.objects.filter(username=request.session.get("info")['username']).first()
     if user_obj.is_admin:
         # 只有管理员用户才能看见该网页
